4b07688789f370e93d25f0dafac6b112fcd2d478
Aron Strandberg <strandberg.aron@gmail.com>
2017-11-18 17:20:31 +0100

    Remove unused cruft

--- client/src/PlayerSearch.jsx
+++ client/src/PlayerSearch.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 
 import PlayerSearchInput from './PlayerSearchInput'
-import PlayerSearchButton from './PlayerSearchButton'
 import PlayerSearchResults from './PlayerSearchResults'
 import './PlayerSearch.css'
 
@@ -14,12 +13,10 @@ class PlayerSearch extends Component {
 
   onChange = (event) => {
     const value = event.target.value;
-    console.log(value)
     this.setState({ value: value });
   }
 
   onSubmit = (event) => {
-    console.log("hejje")
     event.preventDefault();
     this.search();
   }
@@ -51,7 +48,6 @@ class PlayerSearch extends Component {
       <div className="player player-search">
         <form onSubmit={this.onSubmit}>
           <PlayerSearchInput value={this.state.value} onChange={this.onChange} />
-          {/*<PlayerSearchButton onClick={this.search} />*/}
         </form>
         { this.state.displayResults &&
           <PlayerSearchResults tracks={this.state.tracks} onClick={this.onSelect} />
