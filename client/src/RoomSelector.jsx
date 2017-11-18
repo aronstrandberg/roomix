f0933716a8eebdbfab35e57f9360cd7c70f8cf60
Aron Strandberg <strandberg.aron@gmail.com>
2017-11-18 22:00:44 +0100

    Fetch actual rooms from the database

--- client/src/RoomSelector.jsx
+++ client/src/RoomSelector.jsx
@@ -4,9 +4,19 @@ import Room from './Room'
 import RoomSelectorCreateRoom from './RoomSelectorCreateRoom'
 import './RoomSelector.css'
 
+import { getRooms } from './services'
+
 class RoomSelector extends Component {
+  state = {
+    rooms: []
+  }
+  componentDidMount() {
+    getRooms().then(result => {
+      this.setState({ rooms: result.rooms })
+    })
+  }
   render() {
-    const { rooms } = this.props;
+    const { rooms } = this.state;
     return (
       <div className="room-selector">
         <h2>Join a room</h2>
